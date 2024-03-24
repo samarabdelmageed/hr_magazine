0df9fd34a7619424f37145816a219250a822ecb8
samarabdelmageed <samarabdelmageed@yahoo.com>
2024-03-24 23:45:58 +0200

    hr_magazine

---
+++ app/Models/Event.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Event extends Model
+{
+    use HasFactory, HasSlug;
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+
+    protected $fillable = [
+        'title',
+        'from',
+        'to',
+        'image',
+        'streetNo',
+        'city',
+        'state',
+        'postalCode',
+        'country',
+        'latitude',
+        'longitude',
+        'description',
+        'speakers',
+        ];
+
+    public function agenda(){
+        return $this->hasOne(Agenda::class);
+    }
+}
---
+++ database/migrations/2024_03_24_045251_create_agendas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->references('id')->on('events')->onDelete('cascade')->onUpdate('cascade');
+            $table->longText('content');
+            $table->time('from');
+            $table->time('to');
+            $table->text('speakers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agendas');
+    }
+};
